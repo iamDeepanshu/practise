215ee5983514b2a0c11efc783cd3f01a763be099
Deepanshu Saini <avideepanshu99@gmail.com>
2023-06-14 23:46:26 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+class sum{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        
+        System.out.println("sum of  " + a + " and " + b +" is:");
+        System.out.println(a+b);
+    }
+}
