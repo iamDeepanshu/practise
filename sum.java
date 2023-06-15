d4fc237208d4aaacd50b08dbf7980c800f4bf371
Deepanshu Saini <avideepanshu99@gmail.com>
2023-06-16 01:15:45 +0530

    taken input from user

--- sum.java
+++ sum.java
@@ -1,7 +1,14 @@
+import java.util.*;
+
 class sum{
     public static void main(String[] args) {
-        int a=10;
-        int b=20;
+        int a;
+        int b;
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter first number");
+        a=s.nextInt();
+        System.out.println("enter second number");
+        b=s.nextInt();
         
         System.out.println("sum of  " + a + " and " + b +" is:");
         System.out.println(a+b);
